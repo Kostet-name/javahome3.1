74fa077147f6fe852179a459879c9cdb59fbf243
Konstantin <kostenn07@mail.ru>
2023-03-19 15:13:14 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int miles = price / 20;
+        return miles;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+    }
+}
